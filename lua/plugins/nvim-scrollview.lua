1445d6ff70c2732f701522927d1ddac899992cb1
skyler <skyler.wan@dreo.com>
2026-07-18 10:59:17 +0800

    feat: scrollview 右侧显示 gitsigns 增改删标记
    更新插件锁定版本

--- lua/plugins/nvim-scrollview.lua
+++ lua/plugins/nvim-scrollview.lua
@@ -1,7 +1,12 @@
--- 文件滚动条插件
+-- 文件滚动条插件，右侧显示 gitsigns 增改删标记
 return {
   {
     "dstein64/nvim-scrollview",
+    dependencies = { "lewis6991/gitsigns.nvim" },
     opts = {},
+    config = function(_, opts)
+      require("scrollview").setup(opts)
+      require("scrollview.contrib.gitsigns").setup()
+    end,
   },
 }
