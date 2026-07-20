33c678e337c6fccadcfc6d4a475d4956ab50db59
skyler <skyler.wan@dreo.com>
2026-07-20 10:24:06 +0800

    fix: gitsigns 标记同行按优先级去重，避免叠加显示

--- lua/plugins/nvim-scrollview.lua
+++ lua/plugins/nvim-scrollview.lua
@@ -5,8 +5,14 @@ return {
     dependencies = { "lewis6991/gitsigns.nvim" },
     opts = {},
     config = function(_, opts)
+      -- 同一行只保留一个 gitsigns 标记，避免 add/change/delete 叠在一起
+      vim.g.scrollview_signs_max_per_row_by_group = { gitsigns = 1 }
       require("scrollview").setup(opts)
-      require("scrollview.contrib.gitsigns").setup()
+      require("scrollview.contrib.gitsigns").setup({
+        add_priority = 90,
+        change_priority = 91,
+        delete_priority = 92,
+      })
     end,
   },
 }
